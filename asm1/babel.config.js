69f7cf8481cf53ad03699345dcf6fc95023d7839
tuna26hn <tuna26hn@gmail.com>
2026-06-02 00:12:22 +0700

    chore: update Babel configuration and remove unused dependencies

--- asm1/babel.config.js
+++ asm1/babel.config.js
@@ -1,14 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+      ["@babel/plugin-proposal-private-methods", { loose: true }],
+      ["@babel/plugin-transform-class-properties", { loose: true }],
       [
-        'module-resolver',
+        "module-resolver",
         {
-          root: ['./'],
+          root: ["./"],
           alias: {
-            '@': './',
+            "@": "./",
           },
         },
       ],
--- asm1/index.js
+++ asm1/index.js
@@ -1,6 +1,15 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+
+if (typeof global.DOMException === "undefined") {
+  global.DOMException = class DOMException extends Error {
+    constructor(message, name) {
+      super(message);
+      this.name = name;
+    }
+  };
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
